ad3f31dd8207adb3ea2e2108bec64cd7e26e0b83
nildoneumann <leonildoneumann@gmail.com>
2026-03-02 21:05:55 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,29 @@
-const CACHE = "investpro-v4";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-        "./comparador.html",
-      ]);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install", event => {
+  self.skipWaiting(); // ativa imediatamente
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE)
-        .map(k => caches.delete(k))
-      );
-    })
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim()); // assume controle imediato
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request)
-    .then(res => res || fetch(e.request))
+/* Estratégia: Network First */
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open("dynamic-cache")
+          .then(cache => cache.put(event.request, clone));
+
+        return response;
+
+      })
+      .catch(() => caches.match(event.request))
   );
 
 });
